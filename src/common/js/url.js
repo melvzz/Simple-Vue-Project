aaf7a4878968112c2733ee691c8e718239d16d25
melvzz <277514786@qq.com>
2017-03-28 09:51:12 +0800

    Add files via upload

---
+++ src/common/js/url.js
@@ -0,0 +1,24 @@
+/**
+ * 解析 url 参数
+ * @example ?id=12345&a=b
+ * @return Object {id:12345, a:b}
+ */
+
+export function urlParse(){
+  let url = window.location.search;
+  let obj = {};
+  let reg = /[?&][^?&]+=[^?&]+/g;
+  let arr = url.match(reg);
+  // ['?id=12345', '&a=b']
+  if (arr){
+    arr.forEach(function(item){
+      let tempArr = item.substring(1).split('=');
+      let key = decodeURIComponent(tempArr[0]);
+      let val = decodeURIComponent(tempArr[1]);
+      obj[key] = val;
+    });
+  }
+  return obj;
+}
+
+
